30b60e2e84091360d901ab28b7492ad783f40506
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r4s2.42madrid.com>
2024-05-08 11:21:53 +0200

    added print when there is no args

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:34:46 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/04/24 12:26:54 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2024/05/08 11:19:47 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 int main(int argc, char **argv)
 {
+	int	j;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
 	for (int i = 1; i < argc; i++)
-	{	
-		for (size_t j = 0; j < strlen(argv[i]); j++)
-		{			
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
 			if (isalpha(argv[i][j]))
 				std::cout << (char) toupper(argv[i][j]);
     		else
-				std::cout << argv[i][j];				
+				std::cout << argv[i][j];
+			j++;
 		}
 		std::cout << " ";
 	}
