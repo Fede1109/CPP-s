0c01d1b73545efcc3d280c98d40d3033af7cb08d
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r3s6.42madrid.com>
2024-05-13 14:57:20 +0200

    starting ft_replace on ex04

--- cpp01/ex04/Sed.cpp
+++ cpp01/ex04/Sed.cpp
@@ -1,5 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sed.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 14:14:14 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2024/05/13 14:44:14 by fdiaz-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "Sed.hpp"
+
+void    Sed::replace (std::string s1, std::string s2)
+{
+    std::cout << s1 << std::endl;
+    std::cout << s2 << std::endl;
+}
 
 Sed::Sed(std::string filename) : infile(filename){
     this->outfile = this->infile + ".replace";
--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Sed.hpp                                            :+:      :+:    :+:   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/13 14:14:22 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/05/13 14:14:23 by fdiaz-gu         ###   ########.fr       */
+/*   Created: 2024/05/13 14:15:46 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2024/05/13 14:15:47 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#ifndef SED_HPP
-#define SED_HPP
-#include <iostream>
-class Sed
-{
-	private:
-		std::string outfile;
-		std::string infile;
-	public:
-		Sed( std::string filename );
-		~Sed();
-		void	replace( std::string s1, std::string s2);
-};
-#endif
