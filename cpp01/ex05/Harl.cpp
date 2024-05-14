50a8fd56f82c58c72cf4bb80deb61d1676d89331
Federico Díaz <fede@iMac-de-Wilo.local>
2024-05-14 08:12:29 +0200

    Starting ex05

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -3,10 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   Harl.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fede <fede@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:15:46 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/05/13 14:15:47 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2024/05/13 18:07:18 by fede             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+void    Harl::debug( void ) {
+    std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void    Harl::info( void ) {
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void    Harl::warning( void ) {
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming foryears whereas you started working here since last month." << std::endl;
+}
+
+void    Harl::error( void ) {
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+Harl::Harl(){}
+
+Harl::~Harl(){}
