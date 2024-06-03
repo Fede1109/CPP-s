843cc851aefd6d6f1c42c5d39d40ced965ad7ac6
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r3s4.42madrid.com>
2024-06-03 18:47:04 +0200

    starting ex02

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 16:12:17 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2024/06/03 15:35:36 by fdiaz-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(){
+    std::cout << "Default constructor called" << std::endl;
+    this->_num = 0;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+
+Fixed::Fixed(const int num) : _num(num << _bits)
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float num): _num(std::roundf((num) * (1 << _bits)))
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
+
+Fixed::Fixed(const Fixed &f)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = f;
+    
+}
+
+Fixed&    Fixed::operator= (const Fixed &a)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if ( this != &a )
+            this->_num = a.getRawBits();
+        return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (_num);
+}
+
+void Fixed::setRawBits(int const raw)
+{    
+    _num = raw;
+}
+
+float   Fixed::toFloat( void )  const
+{
+    return (float)((getRawBits()) / (1 << _bits));
+}
+
+int Fixed::toInt( void ) const
+{
+    return (getRawBits() >> _bits);
+}
+
+std::ostream& operator<<(std::ostream &os, Fixed const &a)
+{
+    os << a.toFloat();
+    return os;
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 16:11:41 by fdiaz-gu          #+#    #+#             */
+/*   Updated: 2024/06/03 18:45:45 by fdiaz-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iostream>
+#include <cmath>
+class Fixed
+{
+private:
+    int _num;
+    static const int _bits = 8;
+public:
+    Fixed();
+    ~Fixed();
+    Fixed(const int num);
+    Fixed(const float num);
+    Fixed(const Fixed &f);
+//  ******COMPARISON OPERATORS******
+    Fixed&    operator= (const Fixed &a);
+    Fixed&    operator> (const Fixed &a);
+    Fixed&    operator>= (const Fixed &a);
+    Fixed&    operator<= (const Fixed &a);
+    Fixed&    operator== (const Fixed &a);
+    Fixed&    operator!= (const Fixed &a);
+
+//  ******ARITHMETIC OPERATORS******
+    Fixed&    operator+ (const Fixed &a);
+    Fixed&    operator- (const Fixed &a);
+    Fixed&    operator* (const Fixed &a);
+    Fixed&    operator/ (const Fixed &a);
+    
+//  ******INCREMENT/DECREMENT OPERATORS******
+    Fixed&    operator++ ();
+    Fixed&    operator++ (int);
+    Fixed&    operator-- ();
+    Fixed&    operator-- (int);
+
+//  ******OVERLOADED FUNCTIONS******
+
+    Fixed&  min(Fixed &f1, Fixed &f2);
+    Fixed& min(const Fixed& f1, const Fixed& f2);
+    
+    float   toFloat ( void ) const;
+    int     toInt ( void ) const;
+    int getRawBits(void) const;
+    void    setRawBits(int const raw);
+
+};
+std::ostream& operator<<(std::ostream& os, Fixed const &a);
+
+#endif
