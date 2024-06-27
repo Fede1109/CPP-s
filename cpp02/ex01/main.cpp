1fcc5e5c24c4e3914127fe93147b8063a2e53376
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r5s5.42madrid.com>
2024-06-27 17:24:09 +0200

    makefiles

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,25 +6,26 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 16:11:32 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2024/05/30 17:37:15 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2024/06/27 17:16:12 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void ) {
+int main(void)
+{
     Fixed a;
-    Fixed const b( 10 );
-    Fixed const c( 42.42f );
-    Fixed const d( b );
-    a = Fixed( 1234.4321f );
+    // Fixed const b(10);
+    // Fixed const c(42.42f);
+    // Fixed const d(b);
+    a = Fixed(1234.4321f);
     std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    // std::cout << "b is " << b << std::endl;
+    // std::cout << "c is " << c << std::endl;
+    // std::cout << "d is " << d << std::endl;
+    // std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    // std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    // std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    // std::cout << "d is " << d.toInt() << " as integer" << std::endl;
     return 0;
 }
