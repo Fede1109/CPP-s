dbdf5295ba9cbbe8dc848dfcc0ebc3b5c405902a
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r15s2.42madrid.com>
2024-07-04 11:39:41 +0200

    updated main

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -3,18 +3,16 @@
 int main(void)
 {
 	DiamondTrap duko("duko");
-	// duko.guardGate();
-	// duko.highFivesGuys();
-	std::cout << "---------------" << std::endl;
+	duko.guardGate();
+	duko.highFivesGuys();
 	duko.whoAmI();
 	duko.attack("pepe");
 	duko.guardGate();
-	std::cout << "---------------" << std::endl;
 
-	// DiamondTrap ysy;
-	// ysy.whoAmI();
-	// ysy = duko;
-	// ysy.whoAmI();
-	// ysy.attack("ysy");
+	DiamondTrap ysy;
+	ysy.whoAmI();
+	ysy = duko;
+	ysy.whoAmI();
+	ysy.attack("ysy");
 	return (0);
 }
