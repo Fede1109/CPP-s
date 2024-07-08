24eb081567d7bac4daaa950430f4172e04461418
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2024-07-08 13:10:45 +0200

    defining member functions

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -1,3 +1,30 @@
 #include "Character.hpp"
 
+Character::Character(void)
+{
+	std::cout << "Default Character constructor called" << std::endl;
+	this->_name = "Default Character";
+}
 
+Character::~Character (void)
+{
+	std::cout << "Character destructor called" << std::endl;
+}
+
+Character& Character::operator=(const Character& c)
+{
+	this->_name = c._name;
+	return *this;
+}
+Character::Character(const Character& c)
+{
+	*this = c;
+}
+std::string const&  Character::getName(void) const
+{
+	return this->_name;
+}
+void Character::equip(AMateria* m)
+{
+
+}
--- cpp04/ex03/Character.hpp
+++ cpp04/ex03/Character.hpp
@@ -1,4 +1,22 @@
 #ifndef CHARACTER_HPP
 #define CHARACTER_HPP
 
+#include "./ICharacter.hpp"
+
+class Character: public ICharacter
+{
+	private:
+	std::string _name;
+	AMateria *materias[4];
+
+public:	
+	Character(void);
+	Character(const Character& c);
+	Character &operator=(const Character& c);
+	~Character(void);
+	std::string const& getName() const;
+	void equip(AMateria* m);
+	void unequip(int idx);
+	void use (int idx, ICharacter& target);
+};
 #endif
