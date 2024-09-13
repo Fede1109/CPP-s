558c36f72adf9988b4b284d87e0f12361e204a6c
Federico Díaz <federicojose2000@gmail.com>
2024-09-13 20:10:25 +0200

    .hpP

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fede <fede@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 19:28:40 by fede              #+#    #+#             */
+/*   Updated: 2024/09/13 19:53:40 by fede             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+private:
+	const std::string _name;
+	int _grade;
+
+public:
+	Bureaucrat(void);
+	Bureaucrat(Bureaucrat &b);
+	Bureaucrat(std::string name, int grade);
+	~Bureaucrat(void);
+	std::string getName(void) const;
+	int getGrade(void) const;
+	void incrementGrade(void);
+	void decrementGrade(void);
+
+	Bureaucrat &operator=(const Bureaucrat &b);
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual char const *what(void) const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual char const *what(void) const throw();
+	};
+};
+std::ostream &operator<<(std::ostream& stream, Bureaucrat &b);
+#endif
