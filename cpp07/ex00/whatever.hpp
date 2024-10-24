3f95165851b544aba77c2ce01470fa3f06de091d
Federico Díaz <federicojose2000@gmail.com>
2024-10-24 13:16:43 +0200

    ex00 almost don

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -1,21 +1,23 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
-
+#include <iostream>
 template <typename T>
 void swap (T &a, T &b)
 {
-
+	T c = a;
+	a = b;
+	b = c;
 }
 
 template <typename T>
-T min (T a, T b)
+T min (T &a, T &b)
 {
-
+	return a < b ? a : b; 
 }
 
 template <typename T>
-T max (T a, T b)
+T max (T &a, T &b)
 {
-
+	return a > b ? a : b;
 }
 #endif
