b93f4654a9d28a05c0a76b7291afdaf7ae682fd6
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2024-10-24 14:50:42 +0200

    addin prototipes

--- cpp06/ex00/ScalarConvert.hpp
+++ cpp06/ex00/ScalarConvert.hpp
@@ -21,9 +21,19 @@ public:
 	static void convert(std::string data);
 };
 int check_args(std::string data);
-int	get_type(std::string data);
+int get_type(const std::string &data, int length);
 void print_conversion(int type, std::string data);
+int check_int(const std::string &data);
 void convert_int(std::string data);
+void convert_char(std::string data);
+void convert_double(std::string data);
+void convert_float(std::string data);
+int check_nan(std::string data);
+int check_inf(std::string data);
+void handle_pseudo(std::string data);
+float int_to_float(long num);
+double int_to_double(long num);
+
 enum DataType {
     TYPE_UNRECOGNIZED = 0,
     IS_INT = 1,
