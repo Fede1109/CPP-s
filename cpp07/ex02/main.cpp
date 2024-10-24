a979675685141e8886582f30105ef65d9426778d
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2024-10-24 18:39:48 +0200

    done

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -3,11 +3,14 @@
 int main (void)
 {
 	Array<int> data(5);
+	Array<int> arr(3);
 	data[2] = 2;
+
 	std::cout << data[2] << std::endl;
 	try
 	{
 		data[10];
+		std::cout << data[10] << std::endl;
 	}
 	catch(const std::exception& e)
 	{
