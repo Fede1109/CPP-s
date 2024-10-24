b0576d9f8ffea904dec05e3ebb67b8c1af13f638
Fede <federicojose2000@gmail.com>
2024-10-24 11:24:03 +0200

    starting cpp07

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,21 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap (T &a, T &b)
+{
+
+}
+
+template <typename T>
+T min (T a, T b)
+{
+
+}
+
+template <typename T>
+T max (T a, T b)
+{
+
+}
+#endif
