9bfa43f901a75cd265820f82bdf82cadd73a2e45
Federico Díaz <federicojose2000@gmail.com>
2024-10-24 13:17:50 +0200

    done ex00

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -10,13 +10,13 @@ void swap (T &a, T &b)
 }
 
 template <typename T>
-T min (T &a, T &b)
+T &min (T &a, T &b)
 {
 	return a < b ? a : b; 
 }
 
 template <typename T>
-T max (T &a, T &b)
+T &max (T &a, T &b)
 {
 	return a > b ? a : b;
 }
