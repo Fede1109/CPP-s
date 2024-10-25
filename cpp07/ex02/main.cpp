19e3d744375fa7120681224cb0c51c972a2f1ece
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2024-10-25 13:20:02 +0200

    done ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,32 +1,44 @@
 #include "Array.hpp"
 
-int main (void)
+int main(void)
 {
 	Array<int> data(5);
-	Array<int> tmp = data;
-	Array<int> arr(3);
-	data[2] = 2;
+	for (size_t i = 0; i < 5; i++)
+	{
+		data[i] = i;
+	}
+	
+	Array<int> tmp = data;	
 
 	for (size_t i = 0; i < 5; i++)
 	{
 		if (data[i] != tmp[i])
 		{
-			std::cout << data[i] << std::endl;
-			std::cout << tmp[i] << std::endl;
-			std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
+			std::cerr << "Didn't save the same value!!" << std::endl;
+			break;
 		}
 	}
-	
+	data[2] = 20;
+	for (size_t i = 0; i < 5; i++)
+	{
+		std::cout << "Data["<< i << "]: " << data[i] << std::endl;
+		std::cout << "tmp ["<< i << "]: "  << tmp[i] << std::endl;
+		if (data[i] != tmp[i])
+		{
+			std::cerr << "Didn't save the same value!!" << std::endl;
+			break;
+		}
+	}
+
 	std::cout << data[2] << std::endl;
 	try
 	{
 		data[10];
 		std::cout << data[10] << std::endl;
 	}
-	catch(const std::exception& e)
+	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
-	}	
+	}
 	return (0);
 }
