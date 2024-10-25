0970d890b1781d778bbc67d4f4549b9b1b2ef5a3
Federico Díaz <federicojose2000@gmail.com>
2024-10-25 11:49:19 +0200

    not done yet

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,51 +2,60 @@
 #define ARRAY_HPP
 #include <iostream>
 
-template<typename T>
+template <typename T>
 class Array
 {
-	private:
-		T *_arr;
-		unsigned int _len;
-	public:
-		Array(void) : _arr(new T[0]), _len(0){};
-		Array(unsigned int n)
-		{
-			this->_len = n;
-			this->_arr = new T[n];
-			for (unsigned int i = 0; i < n; i++)
-			{
-				this->_arr[i] = T();
-			}
-		}
-		~Array(void)
-		{
-			delete [] this->_arr;
-		}
-		Array(const Array &arr)
-		{
-			*this = arr;
-		}
-		Array& operator=(const Array &arr)
-		{
-			this->_len = arr._len;
-			this->_arr = arr._arr;
-			for (unsigned int i = 0; i < arr._len; i++)
-			{
-				this->_arr[i] = arr.arr[i];
-			}
-			return *this;
-		}
-		T &operator[](unsigned int i)
-		{
-			if (i > this->_len)
-				throw std::out_of_range("Index is out of bounds");
-			return this->_arr[i];
-		}
-		int size(void) const
-		{
-			return this->_len;
-		}
+private:
+	T *_arr;
+	unsigned int _len;
 
+public:
+	Array(void) : _arr(new T[0]), _len(0) {};
+	Array(unsigned int n)
+	{
+		this->_len = n;
+		this->_arr = new T[n];
+		for (unsigned int i = 0; i < n; i++)
+		{
+			this->_arr[i] = T();
+		}
+	}
+	~Array(void)
+	{
+		if (this->_arr != 0)
+			delete[] this->_arr;
+	}
+	Array(const Array &arr): _arr(0), _len(0)
+	{
+		*this = arr;
+	}
+	Array &operator=(const Array &arr)
+	{
+		if (this != &arr)
+		{
+			if (this->_arr != 0)
+				delete[] this->_arr;
+			this->_len = arr.size();
+			this->_arr = new T[this->_len];
+			if (this->_arr)
+			{
+				for (unsigned int i = 0; i < arr._len; i++)
+				{
+					this->_arr[i] = arr._arr[i];
+				}
+			}
+		}
+		return *this;
+	}
+	T &operator[](unsigned int i)
+	{
+		if (i > this->_len - 1 || i < 0)
+			throw std::out_of_range("Index is out of bounds");
+		return this->_arr[i];
+	}
+	int size(void) const
+	{
+		return this->_len;
+	}
 };
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -3,9 +3,21 @@
 int main (void)
 {
 	Array<int> data(5);
+	Array<int> tmp = data;
 	Array<int> arr(3);
 	data[2] = 2;
 
+	for (size_t i = 0; i < 5; i++)
+	{
+		if (data[i] != tmp[i])
+		{
+			std::cout << data[i] << std::endl;
+			std::cout << tmp[i] << std::endl;
+			std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+		}
+	}
+	
 	std::cout << data[2] << std::endl;
 	try
 	{
