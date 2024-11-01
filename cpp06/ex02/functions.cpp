b5d629ae8980d87a57ba3f22046af7b830ce85ec
Federico Díaz <federicojose2000@gmail.com>
2024-11-01 22:45:16 +0100

    fixing ex02

--- cpp06/ex02/functions.cpp
+++ cpp06/ex02/functions.cpp
@@ -27,34 +27,50 @@ Base *generate(void)
 
 void identify(Base *p)
 {
-    if (A *a = dynamic_cast<A *>(p))
-        std::cout << "Class A" << std::endl;
-    else if (B *b =dynamic_cast<B*>(p))
-        std::cout << "Class B" << std::endl;
-    else if (C *c = dynamic_cast<C*>(p))
-        std::cout << "Class C" << std::endl;
-    else
-        std::cerr << "Unknown type" << std::endl;
+	if (A *a = dynamic_cast<A *>(p))
+		std::cout << "Class A" << std::endl;
+	else if (B *b = dynamic_cast<B *>(p))
+		std::cout << "Class B" << std::endl;
+	else if (C *c = dynamic_cast<C *>(p))
+		std::cout << "Class C" << std::endl;
+	else
+		std::cerr << "Unknown type" << std::endl;
 }
 
-void identify(Base &p){
-	try{
+void identify(Base &p)
+{
+	try
+	{
 		A &a = dynamic_cast<A &>(p);
 		(void)a;
 		std::cout << "Class A" << std::endl;
 		return;
-	}catch(std::exception &e){};
-	try{
-		B b = dynamic_cast<B &>(p);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	};
+	try
+	{
+		B &b = dynamic_cast<B &>(p);
 		(void)b;
 		std::cout << "Class B" << std::endl;
 		return;
-	}catch(std::exception &e){};
-	try{
-		C c = dynamic_cast<C &>(p);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	};
+	try
+	{
+		C &c = dynamic_cast<C &>(p);
 		(void)c;
 		std::cout << "Class C" << std::endl;
 		return;
-	}catch(std::exception &e){};
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	};
 	std::cerr << "unknown reference" << std::endl;
 }
